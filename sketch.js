0a828ae4264cd219aba613d9b62480ade9b533c4
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2021-03-01 13:56:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,22 +5,24 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint=Matter.Constraint;
-
-var tree, stone,ground, launcher,mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12, world,boy;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
+var world,boy;
 var launchingForce=100;
 
 function preload(){
-	boy=loadImage("boy.png");
+	boy=loadImage("images/boy.png");
   }
 
 function setup() {
 	createCanvas(1300, 600);
 	engine = Engine.create();
 	world = engine.world;
-	stone=new stone(235,420,30); 
+
+	stoneObj=new stone(235,420,30); 
 
 	mango1=new mango(1100,100,30);
-        mango2=new mango(1170,130,30);
+  mango2=new mango(1170,130,30);
 	mango3=new mango(1010,140,30);
 	mango4=new mango(1000,70,30);
 	mango5=new mango(1100,70,30);
@@ -128,4 +130,3 @@ function keyPressed() {
     }
 
   }
-
